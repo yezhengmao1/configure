19160dd8c45a6699477c41a730489b4c3eaccd2f
Pipapa <yezhengmaolove@gmail.com>
2017-01-31 16:21:37 +0000

    Remove lightDM

--- scripts/install_Step4.sh
+++ scripts/install_Step4.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-#图形界面安装
-#Warning:确保驱动安装正确
 echo "--Install desktop--"
 pacman -S xorg-server xorg-server-utils xorg-xinit xorg-xinit xorg-utils xterm \
 i3lock i3status i3-wm dmenu feh sakura lightdm fcitx fcitx-im fcitx-configtool \
-fcitx-sogoupinyin
+networkmanager
 echo "done!"
 echo "--Config desktop--"
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
-systemctl enable lightdm.service
 echo "exec i3" > ~/.xinitrc
-echo "export GTK_IM_MODULE=fcitx" > ~/.xprofile
-echo "export QT_IM_MODULE=fcitx" > ~/.xprofile
-echo "export XMODIFIERS="@im=fcitx"" > ~/.xprofile
