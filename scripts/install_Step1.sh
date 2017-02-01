3eb2c3d7a4397dc29c315f1360594690e0e642a4
Pipapa <yezhengmaolove@gmail.com>
2017-02-01 20:18:52 +0000

    构建基础系统

--- scripts/install_Step1.sh
+++ scripts/install_Step1.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-#Warning:挂载好硬盘后食用！
-echo "--Install ArchLinux Step.1--"
-echo "--Add mirrorlist--"
-mv /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
-echo "Server = http://mirrors.aliyun.com/archlinux/$repo/os/arch" > /etc/pacman.d/mirrorlist
-echo "done!"
-echo "-----------------------------------------------"
-echo "--Install base system--"
+#Warning!!After Mount /mnt /mnt/home /mnt/boot/EFI(UEFI) 
+echo "--Install ArchLinux Basic System--"
+echo "--Add Mirrorlist--"
+Server="Server = http://mirrors.aliyun.com/archlinux/\$repo/os/arch"
+File=/etc/pacman.d/mirrorlist
+cp ${File} ${File}.bak
+echo ${Server} >> ${File} 
+echo "--Step.1 Done!--"
+echo "--Install Base System--"
 pacstrap -i /mnt base base-devel
-echo "done!"
-echo "-----------------------------------------------"
+echo "--Step.2 Done!--"
 echo "--Generate fstab--"
-genfstab -U -p /mnt > /mnt/etc/fstab
-echo "done!"
-arch-chroot /mnt /bin/bash
+genfstab -U /mnt > /mnt/etc/fstab
+arch-chroot /mnt /bin/base
