a3a96a2538c4f5e27af27a41a67bedb70a4e40b7
Pipapa <yezhengmaolove@gmail.com>
2017-02-01 20:26:13 +0000

    驱动问题--删除图形化安装

--- scripts/install_Step4.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "--Install desktop--"
-pacman -S xorg-server xorg-server-utils xorg-xinit xorg-xinit xorg-utils xterm \
-i3lock i3status i3-wm dmenu feh sakura lightdm fcitx fcitx-im fcitx-configtool \
-networkmanager
-echo "done!"
-echo "--Config desktop--"
-cp /etc/X11/xinit/xinitrc ~/.xinitrc
-echo "exec i3" > ~/.xinitrc
