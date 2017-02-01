2603962e48de11e7c52a08733b4bf2bcb9d2e053
Pipapa <yezhengmaolove@gmail.com>
2017-02-01 20:25:47 +0000

    常用软件包安装

--- scripts/install_Step3.sh
+++ scripts/install_Step3.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-#常用软件包安装
-echo "--Install packages--"
-pacman -S vim zsh screenfetch iw wqy-microhei alsa-utils xf86-input-synaptics \
-tar unzip zip cmake git ttf-dejavu
+pacman -S vim zsh screenfetch iw wqy-microhei wqy-zenhei alsa-utils xf86-input-synaptics tar unzip zip cmake git ttf-dejavu
