70b0722edef51aba4b70bcb4552c9dd63ca7bfd4
Pipapa <yezhengmaolove@gmail.com>
2017-02-01 20:22:46 +0000

    本地化设置

--- scripts/install_Step2.sh
+++ scripts/install_Step2.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-#常用配置文件安装
-#Warning:安装系统后食用!
-echo "--Install ArchLinux Step.2--"
+#Warning:安装系统后食用! 本地化设置!
+echo "--Localization--"
 echo "--Config Locale--"
-mv /etc/locale.gen /etc/locale.gen.bak
-echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+cp /etc/locale.gen /etc/locale.gen.bak
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 echo "zh_CN.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
@@ -13,13 +12,13 @@ hwclock --systohc --utc
 systemctl enable dhcpcd
 #HOSTNAME自行修改
 HOSTNAME='Arch-Ye'
-echo $HOSTNAME > /etc/hostname
-sed -i '/localhost/s/$/\t'"$HOSTNAME"'/g' /etc/hosts
-echo "done!"
+echo ${HOSTNAME} > /etc/hostname
+sed -i '/localhost/s/$/\t'"${HOSTNAME}"'/g' /etc/hosts
+echo "Done!"
 echo "--------------------------------------------"
 echo "--Install boot loader--"
 pacman -S grub efibootmgr
 grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=ArchLinux --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
-echo "done!"
-echo "Please reboot!"
+echo "Done!"
+echo "Please Umount And Reboot!"
