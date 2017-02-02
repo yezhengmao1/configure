cb76e78a7c940ade43d44ac2e54412838a9b132d
Pipapa <yezhengmaolove@gmail.com>
2017-02-02 19:57:47 +0000

    Vim配置脚本

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,4 +2,12 @@
 #配置文件安装 vim
 cd ..
 cd ./dotfiles
+echo "==!Install Vimrc!=="
+#安装Vundle插件
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+#配色文件
+mkdir $HOME/.vim/colors
+cp solarized.vim $HOME/.vim/colors/
+#配置文件
 cp vimrc $HOME/.vimrc
+echo "==!Done!=="
