cf9c79725fc26f9549e37a43b6dd9f639a76fd77
Pipapa <yezhengmaolove@gmail.com>
2017-02-03 13:51:43 +0000

    GTK主题

--- scripts/ArchLinux_Install3.sh
+++ scripts/ArchLinux_Install3.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pacman -S vim zsh screenfetch iw wqy-microhei wqy-zenhei alsa-utils xf86-input-synaptics tar unzip zip cmake git ttf-dejavu
-#pacman -S xorg-server xorg-server-utils xorg-xinit xorg xorg-utils xterm i3lock i3-wm i3-wm dmenu feh sakura fcitx fcitx-im fcitx-configtool networkmanager network-manager-applet networkmanager-openvpn linux-headers xf86-video-nouveau nvidia xfce4-power-manager xfce4-terminal 
+#pacman -S xorg-server xorg-server-utils xorg-xinit xorg xorg-utils xterm i3lock i3-wm i3-wm dmenu feh sakura fcitx fcitx-im fcitx-configtool networkmanager network-manager-applet networkmanager-openvpn linux-headers xf86-video-nouveau nvidia xfce4-power-manager xfce4-terminal numix-themes 
 #cp /etc/X11/xinit/xinitrc ~/.xinitrc
