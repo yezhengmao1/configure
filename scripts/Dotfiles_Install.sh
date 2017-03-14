b62f5e0b2d5299862f0e220e7f46729c53202b7b
Pipapa <yezhengmaolove@gmail.com>
2017-03-14 19:36:20 +0800

    修改错误

--- scripts/Dotfiles_Install.sh
+++ scripts/Dotfiles_Install.sh
@@ -2,12 +2,12 @@
 cd ..
 cd ./dotfiles
 
+mkdir $HOME/.vim
 mkdir $HOME/.vim/colors
 cp solarized.vim $HOME/.vim/colors/
-#配置文件安装
 cp config-vim $HOME/.vimrc
 cp config-polybar $HOME/.config/polybar/config
 cp launch.sh $HOME/.config/polybar/launch.sh
 cp weather $HOME/.config/polybar/weather
-co config-i3 $HOME/.config/i3/config
+cp config-i3 $HOME/.config/i3/config
 echo "==!Done!=="
