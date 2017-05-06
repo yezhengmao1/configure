6eb08118ceea37ef121595692e17b94111850691
pipapa <yezhengmaolove@gmail.com>
2017-05-06 19:48:17 +0800

    add:zsh配置文件

--- scripts/Dotfiles_Install.sh
+++ scripts/Dotfiles_Install.sh
@@ -2,6 +2,7 @@
 cd ../dotfiles
 mkdir -p $HOME/.vim/colors
 cp solarized.vim $HOME/.vim/colors/
+cp config-zsh $HOME/.zshrc
 cp config-vim $HOME/.vimrc
 cp config-urxvt $HOME/.Xresources
 cp config-i3 $HOME/.config/i3/config
