ea46bf68b7ef7429a63701419421dbcef9cbc428
pipapa <yezhengmaolove@gmail.com>
2017-06-10 18:56:53 +0800

    修改安装

--- scripts/Zsh_Install.sh
+++ scripts/Zsh_Install.sh
@@ -2,6 +2,6 @@
 #sudo pacman -S zsh
 git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
-sed -i 's/ZSH_THEME=".*"/ZSH_THEME="af-magic"/g' $HOME/.zshrc
+sed -i 's/ZSH_THEME=".*"/ZSH_THEME="bira"/g' $HOME/.zshrc
 chsh -s /bin/zsh
-echo "Install Oh-My-Zsh [OK]"
+echo "Installed Oh-My-Zsh [OK]"
