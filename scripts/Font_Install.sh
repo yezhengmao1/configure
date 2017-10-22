202ca38345180edd0c2325d98bf654433aab1ac4
Pipapa <yezhengmaolove@gmail.com>
2017-10-22 15:24:13 +0000

    rm

--- scripts/Font_Install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-yaourt -S ttf-monaco ttf-font-awesome nerd-fonts-complete powerline-fonts
-
--- scripts/Zsh_Install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#sudo pacman -S zsh
-git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
-cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
-sed -i 's/ZSH_THEME=".*"/ZSH_THEME="bira"/g' $HOME/.zshrc
-chsh -s /bin/zsh
-echo "Installed Oh-My-Zsh [OK]"
