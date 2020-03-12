ccf1b0c4bf57663bac07ba39c9966e98491c5bb7
Pipapa <yezhengmaolove@gmail.com>
2020-03-12 20:54:29 +0800

    [feat]添加插件安装脚本

--- scripts/vinstall.sh
+++ scripts/vinstall.sh
@@ -1,26 +1,53 @@
 #!/bin/bash
 
 VIM_PATH=${HOME}/.vim
-CACHE_PATH=/tmp/vinstall
 
 PATHOGEN=https://tpo.pe/pathogen.vim
 
 RAINBOW=https://github.com/luochen1990/rainbow.git
+CURSORWORD=https://github.com/itchyny/vim-cursorword
+NERDTREE=https://github.com/preservim/nerdtree
+GITGUTTER=https://github.com/airblade/vim-gitgutter
+TAGBAR=https://github.com/majutsushi/tagbar
+MAKR=https://github.com/inkarkat/vim-mark
+
+PLUGIN_NAMES=(
+    "rainbow" 
+    "cursorword"
+    "nerdtree"
+    "gitgutter"
+    "tagbar"
+    "mark"
+)
+
+PLUGINS=(
+    $RAINBOW 
+    $CURSORWORD
+    $NERDTREE
+    $GITGUTTER
+    $TAGBAR
+    $MARK
+)
 
 # install
 # 安装
 Install() {
     # install pathogen
     # 安装vim插件管理
-    mkdir -p ${VIM_PATH}/autoload ${VIM_PATH}/bundle
+    echo 'install pathogen - manage your runtimepath'
+    mkdir -p ${VIM_PATH}/autoload ${VIM_PATH}/bundle ${VIM_PATH}/plugin
     curl -LSso ${VIM_PATH}/autoload/pathogen.vim ${PATHOGEN}
     
-    echo 'install rainbow plugin'
-    git clone ${RAINBOW} ${CACHE_PATH}/rainbow
-    cp ${CACHE_PATH}/rainbow/plugin/* ${VIM_PATH}/plugin
-    cp ${CACHE_PATH}/rainbow/autoload/* ${VIM_PATH}/autoload
+    for i in ${!PLUGINS[@]}; do
+        echo 'install '${PLUGIN_NAMES[$i]}
+        git clone ${PLUGINS[$i]} ${VIM_PATH}/bundle/${PLUGIN_NAMES[$i]} > /dev/null 2>&1
+    done
 
-    rm -rf ${CACHE_PATH}
+    echo 'install plugin done'
+    echo 'install configuration'
+    cp ./../dotfiles/vim/.vimrc ${HOME}/.vimrc
+    cp ./../dotfiles/vim/plugin/p.vim ${VIM_PATH}/plugin/p.vim
+    echo 'install configuration done'
 }
 
 # uninstall
@@ -28,8 +55,12 @@ Install() {
 UnInstall() {
     echo 'uninstall pathogen'
     rm -rf ${VIM_PATH}/autoload
-    echo 'uninstall plugins'
-    echo 'uninstall myplugins'
+    echo 'uninstall bundle'
+    rm -rf ${VIM_PATH}/bundle
+    echo 'uninstall plugin'
+    rm -rf ${VIM_PATH}/plugin
+    echo 'uninstall profile'
+    rm ${HOME}/.vimrc
 }
 
 case $1 in
