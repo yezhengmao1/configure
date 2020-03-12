696c04cb2eb45d6b58206744d3d60a10f2be26a4
Pipapa <yezhengmaolove@gmail.com>
2020-03-12 21:18:05 +0800

    [fix]pathogen下载路径 -> github

--- scripts/vinstall.sh
+++ scripts/vinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 VIM_PATH=${HOME}/.vim
+CACHE_PATH=/tmp
 
-PATHOGEN=https://tpo.pe/pathogen.vim
-
+PATHOGEN=https://github.com/tpope/vim-pathogen
 RAINBOW=https://github.com/luochen1990/rainbow.git
 CURSORWORD=https://github.com/itchyny/vim-cursorword
 NERDTREE=https://github.com/preservim/nerdtree
@@ -34,21 +34,20 @@ PLUGINS=(
 Install() {
     # install pathogen
     # 安装vim插件管理
-    echo 'install pathogen - manage your runtimepath'
+    echo '\033[33minstall \033[0mpathogen'
     mkdir -p ${VIM_PATH}/autoload ${VIM_PATH}/bundle ${VIM_PATH}/plugin
-    curl -LSso ${VIM_PATH}/autoload/pathogen.vim ${PATHOGEN}
-    
+    git clone ${PATHOGEN}  ${CACHE_PATH}/pathogen > /dev/null 2>&1 &
     for i in ${!PLUGINS[@]}; do
-        echo 'install '${PLUGIN_NAMES[$i]}
+        echo -e '\033[33minstall \033[0m'${PLUGIN_NAMES[$i]}
         git clone ${PLUGINS[$i]} ${VIM_PATH}/bundle/${PLUGIN_NAMES[$i]} > /dev/null 2>&1 &
     done
     wait
-
-    echo 'install plugin done'
-    echo 'install configuration'
+    cp ${CACHE_PATH}/pathogen/autoload/pathogen.vim ${VIM_PATH}/autoload/pathogen.vim
+    echo -e '\033[32m[success]install plugin done!\033[0m'
+    echo -e '\033[33minstall \033[0mconfiguration'
     cp dotfiles/vim/.vimrc ${HOME}/.vimrc
     cp dotfiles/vim/plugin/p.vim ${VIM_PATH}/plugin/p.vim
-    echo 'install configuration done'
+    echo -e '\033[32m[success]install configuration done!\033[0m'
 }
 
 # uninstall
